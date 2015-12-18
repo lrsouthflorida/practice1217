14a8b176310b48f1a1e491efd2579eae3d137ab5
bitcoin34 <lorenrobinson428@gmail.com>
2015-12-18 00:57:34 +0000

    Practice track

---
+++ projects/template/fizzbuzz.py
@@ -0,0 +1,23 @@
+n = 0
+while n < 101:
+    if n % 5 == 0 and n % 3 == 0:
+        print'FizzBuzz'
+    elif n % 3 == 0:
+        print'Fizz'
+    elif n % 5 == 0:
+        print'Buzz'
+        
+    else:
+        print n
+        print 'FizzBuzz counting up to 100'
+        
+        n = n + 1
+        
+        if n >= 101:
+            break
+        
+        
+    
+    
+        
+        
